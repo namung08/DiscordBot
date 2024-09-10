fde248cada12a411bf42d1ecf89723eff0574dc6
namung08 <namung08@gmail.com>
2024-09-10 16:11:19 +0900

    :tada: feat(project): 디스코드 카지노 봇 개발을 위한 spring boot 세팅
     디스코드 카지노 봇 개발을 위한 spring boot 기본 세팅을 하였음.

---
+++ src/main/java/com/namung/cazinou/CazinouApplication.java
@@ -0,0 +1,12 @@
+package com.namung.cazinou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CazinouApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CazinouApplication.class, args);
+  }
+}
---
+++ src/test/java/com/namung/cazinou/CazinouApplicationTests.java
@@ -0,0 +1,11 @@
+package com.namung.cazinou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CazinouApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
