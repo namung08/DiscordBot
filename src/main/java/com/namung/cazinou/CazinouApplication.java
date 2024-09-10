3bc9128d8fc6ecae058f1ec0ab7a11c37b679783
namung08 <namung08@gmail.com>
2024-09-10 23:41:11 +0900

    :sparkles: feat(jda): 서버 실행 시 JDA 설정 실행
     Spring 서버가 실행이 될 때 JDA 설정이 서버 실행이 완료가 되기 전에 완료가 되도록 설정

--- src/main/java/com/namung/cazinou/CazinouApplication.java
+++ src/main/java/com/namung/cazinou/CazinouApplication.java
@@ -1,12 +1,42 @@
 package com.namung.cazinou;
 
+import com.namung.cazinou.infrastructure.config.jda.JDAConfig;
+import com.namung.cazinou.infrastructure.util.jda.MessageListener;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Log4j2
 @SpringBootApplication
+@RequiredArgsConstructor
 public class CazinouApplication {
+
+  private final JDAConfig jdaConfig;
+  private final MessageListener messageListener; // 의존성 주입
 
   public static void main(String[] args) {
     SpringApplication.run(CazinouApplication.class, args);
+  }
+
+  @PostConstruct
+  public void startJDA() throws Exception {
+    log.info("Starting JDA...");
+
+    // JDA를 생성합니다.
+    JDA jda = JDABuilder.createDefault(jdaConfig.getToken())
+      .enableIntents(GatewayIntent.MESSAGE_CONTENT) // 메시지 내용을 읽어오기 위한 권한
+      .setActivity(Activity.playing("개발중...")) // 봇의 상태 메시지 설정
+      .addEventListeners(messageListener) // MessageListener를 추가
+      .build();
+
+    // JDA가 준비될 때까지 대기
+    jda.awaitReady();
+    log.info("JDA is ready!");
   }
 }
