6ad366c4731cfad1d136f9aadd67492087dd467b
namung08 <namung08@gmail.com>
2024-09-11 12:04:34 +0900

    :goal_net: feat(error): 에러 코드 추가

--- src/main/java/com/namung/cazinou/infrastructure/exception/CazinouExceptionCode.java
+++ src/main/java/com/namung/cazinou/infrastructure/exception/CazinouExceptionCode.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum CazinouExceptionCode {
-  CAZINOU_MEMBER_IS_BOT("Cazinou_001", "해당 사용자는 봇 입니다.");
+  CAZINOU_MEMBER_IS_BOT("Cazinou_001", "해당 사용자는 봇 입니다."),
+  NOT_FOUND_SEQUENCE("Cazinou_002", "해당 시퀀스를 찾을 수 없습니다.");
 
   private final String code;
   private final String message;
