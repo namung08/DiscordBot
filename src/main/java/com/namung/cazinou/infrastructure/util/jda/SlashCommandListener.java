c6efbc69d7896a3548fb316f0bf6ad137a259975
namung08 <namung08@gmail.com>
2024-09-12 15:33:19 +0900

    :sparkles: feat(command): 봇이 특정 채팅방의 주제에만 작동하도록 설정
     봇이 채팅방의 주제가 `#cazinou` 만 있을 경우 명령어가 작동하도록 설정

--- src/main/java/com/namung/cazinou/infrastructure/util/jda/SlashCommandListener.java
+++ src/main/java/com/namung/cazinou/infrastructure/util/jda/SlashCommandListener.java
@@ -2,11 +2,15 @@ package com.namung.cazinou.infrastructure.util.jda;
 
 import com.namung.cazinou.infrastructure.util.jda.commands.CommandManager;
 import com.namung.cazinou.infrastructure.util.jda.commands.SlashCommands;
+import com.namung.cazinou.infrastructure.util.jda.message.EmbedUtil;
+import com.namung.cazinou.infrastructure.util.jda.message.MessageModel;
 import com.namung.cazinou.user.service.CazinouUserService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.channel.Channel;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
@@ -34,6 +38,10 @@ public class SlashCommandListener extends ListenerAdapter {
     if (event.getUser().isBot()) {
       return;
     }
+    // 명령어가 입력이 된 체널의 주제가 #cazinou인지 확인
+    if (differentChannelTopic(event)) {
+      return;
+    }
     // 명령어를 입력한 사용자의 정보를 MongoDB에 저장을 해야함
     if (!cazinouUserService.isUserExist(event.getUser())) {
       cazinouUserService.saveUser(event.getUser());
@@ -42,5 +50,40 @@ public class SlashCommandListener extends ListenerAdapter {
     SlashCommands command = commandManager.getCommand(event.getName());
 
     command.execute(event);
+  }
+
+  private Boolean differentChannelTopic(SlashCommandInteractionEvent e) {
+    // 채널 정보 가져오기
+    Channel channel = e.getChannel();
+    if (channel instanceof TextChannel textChannel) {
+      // 채널 주제가 없는 경우 처리
+      if (textChannel.getTopic() == null) {
+        // 이미 응답했는지 확인
+        MessageModel messageModel = MessageModel.builder()
+          .title("채널 주제가 없습니다.")
+          .description(
+            "명령어를 입력하려면 채널 주제를 `#cazinou`를 추가해 주세요."
+          )
+          .build();
+        // Embed 메시지 전송 (비공개 응답)
+        EmbedUtil.sendEmbed(e, messageModel, true);
+        return true;
+      }
+
+      // 채널 주제가 `#cazinou`가 아닌 경우 처리
+      if (!textChannel.getTopic().trim().equals("#cazinou")) {
+        // 이미 응답했는지 확인
+        MessageModel messageModel = MessageModel.builder()
+          .title("채널 주제가 #cazinou가 아닙니다.")
+          .description(
+            "명령어를 입력하려면 채널 주제를 `#cazinou`로 변경해주세요."
+          )
+          .build();
+        // Embed 메시지 전송 (비공개 응답)
+        EmbedUtil.sendEmbed(e, messageModel, true);
+        return true;
+      }
+    }
+    return false;
   }
 }
