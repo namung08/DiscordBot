13867ccac3aed8a1b6711fe1d11d00e7b9f06f74
namung08 <namung08@gmail.com>
2024-09-12 14:34:25 +0900

    :sparkles: feat(service): 서비스 선언 및 구현

---
+++ src/main/java/com/namung/cazinou/user/service/CazinouUserService.java
@@ -0,0 +1,47 @@
+package com.namung.cazinou.user.service;
+
+import net.dv8tion.jda.api.entities.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface CazinouUserService {
+  /**
+   * 유저 정보 저장<br>
+   * JDA의 {@link User} 객체를 받아서 저장한다.
+   * 저장할 데이터는 {@link User#getIdLong()} 이다.
+   * @param user {@link User} 객체
+   * @return 저장의 성공 여부
+   * @throws com.mongodb.MongoTimeoutException if a timeout occurs while waiting for a pooled connection
+   * @throws org.springframework.dao.DataAccessException if a data access exception occurs while performing the operation
+   */
+  String saveUser(User user);
+
+  /**
+   * 유저 중복 여부 확인
+   * @param user {@link User} 객체
+   * @return {@code true} 중복, {@code false} 중복 아님
+   */
+  Boolean isUserExist(User user);
+
+  /**
+   * 유저의 잔액을 변경한다.
+   * @param user {@link User} 객체
+   * @param money 가감될 금액 (음수 가능)
+   * @return 변경 성공 여부
+   */
+  String modificationBalanceByMoney(User user, Long money);
+
+  /**
+   * 출석체크<br>
+   * 출석체크를 하고, 보상을 지급한다. <br>
+   * 보상은 {@link #modificationBalanceByMoney(User, Long)} 메서드를 호출하여 지급한다. <br>
+   * 1일 1회만 보상을 지급한다.<br>
+   *
+   * @see #modificationBalanceByMoney(User, Long)
+   * @param user {@link User} 객체
+   * @return 출석체크 결과
+   */
+  String attendanceCheck(User user);
+
+  Long getUserBalance(User user);
+}
---
+++ src/main/java/com/namung/cazinou/user/service/CazinouUserServiceImpl.java
@@ -0,0 +1,76 @@
+package com.namung.cazinou.user.service;
+
+import com.mongodb.MongoTimeoutException;
+import com.namung.cazinou.infrastructure.exception.CazinouException;
+import com.namung.cazinou.infrastructure.exception.CazinouExceptionCode;
+import com.namung.cazinou.user.model.CazinouUser;
+import com.namung.cazinou.user.repository.CazinouUserRepository;
+import com.namung.cazinou.user.repository.CustomCazinouUserRepository;
+import java.time.LocalDate;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.entities.User;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CazinouUserServiceImpl implements CazinouUserService {
+
+  private final CazinouUserRepository repository;
+  private final CustomCazinouUserRepository customRepository;
+
+  @Override
+  public String saveUser(User user)
+    throws MongoTimeoutException, DataAccessException {
+    repository.save(
+      CazinouUser.builder()
+        .discordId(user.getIdLong())
+        .balance(0L)
+        .lastAttendance(LocalDate.now().minusDays(1L))
+        .build()
+    );
+    return "Success to save user";
+  }
+
+  @Override
+  public Boolean isUserExist(User user) {
+    return repository.existsByDiscordId(user.getIdLong());
+  }
+
+  @Override
+  public String modificationBalanceByMoney(User user, Long money) {
+    CazinouUser cazinouUser = getUserByDiscordId(user.getIdLong());
+    cazinouUser.setBalance(cazinouUser.getBalance() + money);
+    cazinouUser.setLastAttendance(LocalDate.now());
+    return customRepository.updateUser(cazinouUser);
+  }
+
+  @Override
+  public String attendanceCheck(User user) {
+    CazinouUser cazinouUser = getUserByDiscordId(user.getIdLong());
+    if (cazinouUser.getLastAttendance().isBefore(LocalDate.now())) {
+      modificationBalanceByMoney(user, 10000L);
+      return "출석체크 완료! 10000원이 지급되었습니다.";
+    }
+    return "이미 오늘 출석 체크를 하셨습니다.";
+  }
+
+  @Override
+  public Long getUserBalance(User user) {
+    return getUserByDiscordId(user.getIdLong()).getBalance();
+  }
+
+  private CazinouUser getUserByDiscordId(Long discordId) {
+    return repository
+      .findByDiscordId(discordId)
+      .orElseThrow(() ->
+        new CazinouException(
+          HttpStatus.BAD_REQUEST,
+          CazinouExceptionCode.NOT_FOUND_USER
+        )
+      );
+  }
+}
