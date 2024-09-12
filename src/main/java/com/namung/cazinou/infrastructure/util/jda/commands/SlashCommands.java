b98777060912f226f5106f6f8357f911e18b5edf
namung08 <namung08@gmail.com>
2024-09-12 18:32:56 +0900

    :sparkles: feat(command): 일하기 로직 생성
     `/일하기` 로직이 실행이 되면 실패를 할 수 있으며 실패 시 돈이 늘지 않음

--- src/main/java/com/namung/cazinou/infrastructure/util/jda/commands/SlashCommands.java
+++ src/main/java/com/namung/cazinou/infrastructure/util/jda/commands/SlashCommands.java
@@ -2,8 +2,11 @@ package com.namung.cazinou.infrastructure.util.jda.commands;
 
 import com.namung.cazinou.infrastructure.util.jda.message.EmbedUtil;
 import com.namung.cazinou.infrastructure.util.jda.message.MessageModel;
+import com.namung.cazinou.infrastructure.util.jda.message.WorkMessage;
 import com.namung.cazinou.user.service.CazinouUserService;
 import java.awt.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
@@ -62,6 +65,49 @@ class GetBotCraterGinHubCommand implements SlashCommands {
       .authorImageUrl("https://avatars.githubusercontent.com/u/113490780?v=4")
       .build();
     EmbedUtil.sendSuccessEmbedWithHook(h, messageModel);
+  }
+}
+
+@RequiredArgsConstructor
+class WorkCommand implements SlashCommands {
+
+  private final CazinouUserService service;
+
+  @Override
+  public void execute(SlashCommandInteractionEvent e, InteractionHook h) {
+    long lowSuccess = 20L;
+    long success = (long) (Math.random() * 40);
+    EmbedUtil.sendSuccessEmbedWithHook(
+      h,
+      MessageModel.builder()
+        .title("성공 확률: " + lowSuccess + success + "%")
+        .description("돈을 벌고 있습니다. 10초 후에 결과가 나옵니다.")
+        .color(Color.YELLOW)
+        .build()
+    );
+    long rerole = (long) (Math.random() * 100);
+    if (rerole > success) {
+      EmbedUtil.sendSuccessEmbedWithHook(
+        h,
+        MessageModel.builder()
+          .title("실패")
+          .description("돈을 벌지 못했습니다.")
+          .color(Color.RED)
+          .build()
+      );
+      return;
+    }
+    // 10초 지연 후 돈 지급
+    CompletableFuture.runAsync(
+      () -> {
+        // 실제 돈을 지급하는 로직
+        Integer result = service.work(e.getUser());
+        MessageModel resultMessage = WorkMessage.generateWorkRewards()
+          .get(result);
+        EmbedUtil.sendSuccessEmbedWithHook(h, resultMessage);
+      },
+      CompletableFuture.delayedExecutor(10, TimeUnit.SECONDS)
+    );
   }
 }
 
