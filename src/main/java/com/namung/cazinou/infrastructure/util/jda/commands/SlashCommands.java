457a9862f3e542a2e4cfaa648adbe8d2004d0bcc
namung08 <namung08@gmail.com>
2024-09-12 15:39:29 +0900

    :sparkles: feat(command): 커멘드 성공 시 반환 임베드 지정
     슬래시 커맨드를 성공 할 경우 반환 시킬 임베드의 값을 지정하였습니다.

--- src/main/java/com/namung/cazinou/infrastructure/util/jda/commands/SlashCommands.java
+++ src/main/java/com/namung/cazinou/infrastructure/util/jda/commands/SlashCommands.java
@@ -1,25 +1,66 @@
 package com.namung.cazinou.infrastructure.util.jda.commands;
 
+import com.namung.cazinou.infrastructure.util.jda.message.EmbedUtil;
+import com.namung.cazinou.infrastructure.util.jda.message.MessageModel;
+import com.namung.cazinou.user.service.CazinouUserService;
+import java.awt.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 public interface SlashCommands {
   void execute(SlashCommandInteractionEvent e);
 }
 
-class HelloCommand implements SlashCommands {
+@RequiredArgsConstructor
+class GiveMoneyCommand implements SlashCommands {
+
+  private final CazinouUserService cazinouUserService;
 
   @Override
   public void execute(SlashCommandInteractionEvent event) {
-    String name = event.getOption("name").getAsString();
-    event.reply("Hello, " + name + "!").queue();
+    MessageModel messageModel = MessageModel.builder()
+      .title("출석 체크")
+      .description(cazinouUserService.attendanceCheck(event.getUser()))
+      .build();
+    EmbedUtil.sendEmbed(event, messageModel, false);
   }
 }
 
-class GoodbyeCommand implements SlashCommands {
+@Log4j2
+@RequiredArgsConstructor
+class CheckBalanceCommand implements SlashCommands {
+
+  private final CazinouUserService cazinouUserService;
 
   @Override
   public void execute(SlashCommandInteractionEvent event) {
-    event.reply("Goodbye!").queue();
+    MessageModel messageModel = MessageModel.builder()
+      .color(Color.CYAN)
+      .title("잔액 확인")
+      .description(
+        event.getMember().getEffectiveName() +
+        "님의 잔액은: " +
+        cazinouUserService.getUserBalance(event.getUser()) +
+        "원 입니다."
+      )
+      .build();
+    EmbedUtil.sendEmbed(event, messageModel, false);
+  }
+}
+
+@RequiredArgsConstructor
+class GetBotCraterGinHubCommand implements SlashCommands {
+
+  @Override
+  public void execute(SlashCommandInteractionEvent event) {
+    MessageModel messageModel = MessageModel.builder()
+      .color(Color.CYAN)
+      .author("namung08")
+      .authorUrl("https://github.com/namung08")
+      .authorImageUrl("https://avatars.githubusercontent.com/u/113490780?v=4")
+      .build();
+    EmbedUtil.sendEmbed(event, messageModel, false);
   }
 }
 
@@ -27,6 +68,10 @@ class UnknownCommand implements SlashCommands {
 
   @Override
   public void execute(SlashCommandInteractionEvent event) {
-    event.reply("Unknown command: " + event.getName()).queue();
+    MessageModel messageModel = MessageModel.builder()
+      .title("알 수 없는 명령어")
+      .description(event.getName() + "이 명령어는 존재하지 않는 명령어 입니다.")
+      .build();
+    EmbedUtil.sendEmbed(event, messageModel, false);
   }
 }
