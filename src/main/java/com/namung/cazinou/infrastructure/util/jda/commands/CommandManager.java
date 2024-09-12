3e13c6f879eb817a357c28a6d7d6bf53c26f4055
namung08 <namung08@gmail.com>
2024-09-12 15:38:15 +0900

    :art: perf(command): 초기 개발용 커멘드 삭제 및 기능 추가
     초기에 개발의 이해를 위해 만든 코드 삭제 및 출석체크, 잔액확인, 개발자 슬래시 커맨드 추가

--- src/main/java/com/namung/cazinou/infrastructure/util/jda/commands/CommandManager.java
+++ src/main/java/com/namung/cazinou/infrastructure/util/jda/commands/CommandManager.java
@@ -1,12 +1,11 @@
 package com.namung.cazinou.infrastructure.util.jda.commands;
 
+import com.namung.cazinou.user.service.CazinouUserService;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
-import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,10 +13,11 @@ public class CommandManager {
 
   private final Map<String, SlashCommands> commandMap = new HashMap<>();
 
-  public CommandManager() {
+  public CommandManager(CazinouUserService cazinouUserService) {
     // 명령어와 핸들러 등록
-    commandMap.put("hello", new HelloCommand());
-    commandMap.put("goodbye", new GoodbyeCommand());
+    commandMap.put("출석체크", new GiveMoneyCommand(cazinouUserService));
+    commandMap.put("잔액확인", new CheckBalanceCommand(cazinouUserService));
+    commandMap.put("개발자", new GetBotCraterGinHubCommand());
   }
 
   public SlashCommands getCommand(String commandName) {
@@ -25,20 +25,22 @@ public class CommandManager {
   }
 
   public List<CommandData> getCommandDataList() {
-    // 명령어 등록 (hello 명령어 등)
-    CommandData helloCommand = Commands.slash(
-      "hello",
-      "Say hello to a user."
-    ).addOptions(
-      new OptionData(
-        OptionType.STRING,
-        "name",
-        "The name of the person to greet"
-      ).setRequired(true)
+    // 돈줘
+    CommandData giveMoneyCommand = Commands.slash(
+      "출석체크",
+      "출석체크를 합니다."
     );
 
-    CommandData goodbyeCommand = Commands.slash("goodbye", "Say goodbye.");
+    CommandData checkBalanceCommand = Commands.slash(
+      "잔액확인",
+      "잔액을 확인합니다."
+    );
 
-    return List.of(helloCommand, goodbyeCommand);
+    CommandData botCraterInfoCommand = Commands.slash(
+      "개발자",
+      "개발자 정보를 확인합니다."
+    );
+
+    return List.of(giveMoneyCommand, checkBalanceCommand, botCraterInfoCommand);
   }
 }
